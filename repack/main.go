a648c30fb1165505d6cd08324c73497d2e1292d2
Sergey Khoroshavin <skhoroshavin@gmail.com>
2025-07-24 21:31:31 +0200

    Implement mass repack

--- repack/main.go
+++ repack/main.go
@@ -1,28 +1,61 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"os"
 	"path/filepath"
 	"repack/tmj"
 	"repack/tmx"
+
 	"strings"
 )
 
 func main() {
 	if len(os.Args) != 3 {
-		println("Usage: repack <input.tmx> <output-dir>")
+		println("Usage: repack <input-dir> <output-dir>")
 		os.Exit(1)
 	}
 
-	inputTmx := os.Args[1]
+	inputDir := os.Args[1]
 	outputDir := os.Args[2]
 
-	outputName := strings.TrimSuffix(filepath.Base(inputTmx), ".tmx")
+	fmt.Println("Input directory:", inputDir)
+	fmt.Println("Output directory:", outputDir)
 
-	src := tmx.Load(inputTmx)
-	src.Repack(outputName)
-	dst := tmj.ConvertFromTMX(src)
+	err := filepath.Walk(inputDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() || !strings.HasSuffix(path, ".tmx") {
+			return nil
+		}
 
-	src.SaveImages(outputDir)
-	dst.Save(filepath.Join(outputDir, outputName+".tmj"))
+		inputTmx := path
+		fullName := strings.TrimSuffix(strings.TrimPrefix(inputTmx, inputDir), ".tmx")
+		shortName := filepath.Base(fullName)
+
+		outputTmj := filepath.Join(outputDir, fullName+".tmj")
+		tmjDir := filepath.Dir(outputTmj)
+
+		fmt.Println("Processing", strings.TrimPrefix(inputTmx, inputDir),
+			"->", strings.TrimPrefix(outputTmj, outputDir))
+
+		err = os.MkdirAll(tmjDir, 0775)
+		if err != nil {
+			log.Fatalf("Failed to create output directory: %v", err)
+		}
+
+		src := tmx.Load(inputTmx)
+		src.Repack(shortName)
+		dst := tmj.ConvertFromTMX(src)
+
+		src.SaveImages(tmjDir)
+		dst.Save(outputTmj)
+
+		return nil
+	})
+	if err != nil {
+		log.Fatalf("Failed to walk input directory: %v", err)
+	}
 }
